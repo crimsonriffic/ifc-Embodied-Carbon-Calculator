01b6200e30d7600dd85dd5164402070d409408ea
davis <davisczh@gmail.com>
2025-01-22 17:12:03 +0800

    change region

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ dotenv.load_dotenv()
 AWS_ACCESS_KEY = os.getenv('AWS_ACCESS_KEY')
 AWS_SECRET_KEY = os.getenv('AWS_SECRET_KEY')
 S3_BUCKET = os.getenv('S3_BUCKET')
-AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
+AWS_REGION = os.getenv('AWS_REGION', 'ap-southeast-2')
 
 # MongoDB Configuration
 MONGODB_URL = os.getenv('MONGODB_URL', 'mongodb://localhost:27017')
