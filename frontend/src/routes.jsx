12d7ead59bec2d335c132f59770c15f31a3a8bf8
Carina <carinachu22@gmail.com>
2025-03-11 23:47:37 +0800

    ui fix

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,7 +1,6 @@
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProjectPage from "./pages/ProjectPage";
-//import ECBreakdownPage from "./pages/ECBreakdownPage";
 import IfcDialog from "./pages/IfcDialog";
 import CreatePage from "./pages/CreatePage";
 import Layout from "./Layout";
@@ -15,7 +14,6 @@ const routes = [
       { path: "/home", element: <HomePage /> },
       { path: "/createProject", element: <CreatePage /> },
       { path: "/project/:projectName?", element: <ProjectPage /> },
-      //{ path: "/ecbreakdown/:projectName?", element: <ECBreakdownPage /> },
       { path: "/ifc", element: <IfcDialog /> },
       { path: "/draft", element: <DraftPage /> },
     ],
